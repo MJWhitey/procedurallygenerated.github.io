be781912574963215c49172cadf3c720f38cc26a
Matt White <white_y@hotmail.com>
2023-11-29 16:19:09 +1100

    Initialising project dependencies. NextJS, ESLint, ThreeJS

---
+++ pages/document.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
