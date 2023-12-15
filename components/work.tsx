2b33966c189be2b2abfe33156847b86d9edb2924
Matt White <white_y@hotmail.com>
2023-12-15 13:47:07 +1100

    In progress. Fleshing out grid generation.

--- components/work.tsx
+++ components/work.tsx
@@ -2,31 +2,53 @@ import React from "react";
 import styles from "./work.module.css";
 import Data from "../constants/work.json";
 
+const IMAGE_ASSETS_PATH = "/images/";
+
 function WorkGrid() {
+  const GRID_COLUMNS = 3;
+
   const renderRow = (data) => {
-    const row : React.ReactElement[] = [];
-    data.forEach(item => {
-        const element = (
-            <div className={styles.gridItem}>{item.title}</div>
-        );
-        row.push(element);
+    const row: React.ReactElement[] = [];
+    data.forEach((item) => {
+      const element = (
+        <div
+          className={styles.gridItem}
+          style={{
+            backgroundImage: `url(${IMAGE_ASSETS_PATH + "tint_blue_rago.png"})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <h2>{item.title}</h2>
+        </div>
+      );
+      row.push(element);
     });
+
+    // Add spacers
+    console.log(row.length, GRID_COLUMNS);
+    if (row.length < GRID_COLUMNS) {
+      for (let i = 0; i < GRID_COLUMNS - row.length; i++) {
+        row.push(<div className={styles.gridItem}></div>);
+      }
+    }
+
     return <div className={styles.gridRow}>{row}</div>;
   };
 
   const renderRows = (data) => {
-    const rows : React.ReactElement[] = [];
-    let _data : string[] = [];
+    const rows: React.ReactElement[] = [];
+    let _data: string[] = [];
     for (let i = 0; i < data.length; i++) {
       const item = data[i];
       _data.push(item);
-      console.log(i, i % 3 === 0 ,  i === data.length - 1 )
-      if ((i+1) % 3 === 0 || i === data.length - 1) {
+      //console.log(i, i % 3 === 0 ,  i === data.length - 1 )
+      if ((i + 1) % GRID_COLUMNS === 0 || i === data.length - 1) {
         rows.push(renderRow(_data));
         _data = [];
       }
     }
-    return rows;
+    return <div className={styles.workGridContainer}>{rows}</div>;
   };
 
   const rows = renderRows(Data.data);
